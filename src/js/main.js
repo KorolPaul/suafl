5d10e0ecde58f48ac00e7eae2ec37346685bb6a0
Pavel Korol <korol.paul@gmail.com>
2023-10-19 20:24:22 +0300

    header

--- src/js/main.js
+++ src/js/main.js
@@ -25,199 +25,6 @@ ticketsSlider.forEach(el => {
     });
 })
 
-const agendaSlider = document.querySelectorAll('.agenda_grid');
-agendaSlider.forEach(el => {
-    const totalSlidesElement = el.parentElement.parentElement.querySelector('.agenda_slider-counter-total');
-    const activeSlideElemwent = el.parentElement.parentElement.querySelector('.agenda_slider-counter-active');
-
-    const slider = tns({
-        container: el,
-        items: 1,
-        gutter: 8,
-        autoWidth: false,
-        mouseDrag: true,
-        autoplay: false,
-        nav: false,
-        controls: true,
-        controlsPosition: 'bottom',
-        loop: false,
-        responsive: {
-            1024: {
-                disable: true
-            }
-        },
-        onInit: (info) => {
-            if (totalSlidesElement) {
-                totalSlidesElement.innerText = info.slideCount;
-            }
-        }
-    });
-
-    slider.events.on('transitionEnd', (info) => {
-        if (activeSlideElemwent) {
-            activeSlideElemwent.innerText = info.displayIndex;
-        }
-    });
-})
-
-const commentsSlider = document.querySelectorAll('.comments_slider');
-commentsSlider.forEach(el => {
-    const totalSlidesElement = el.parentElement.parentElement.querySelector('.comments_slider-counter-total');
-    const activeSlideElemwent = el.parentElement.parentElement.querySelector('.comments_slider-counter-active');
-
-    const slider = tns({
-        container: el,
-        items: 1,
-        gutter: 24,
-        mouseDrag: true,
-        autoplay: false,
-        nav: false,
-        controlsPosition: 'bottom',
-        controls: true,
-        loop: true,
-        responsive: {
-            1070: {
-                gutter: 74,
-            }
-        },
-        onInit: (info) => {
-            if (totalSlidesElement) {
-                totalSlidesElement.innerText = info.slideCount;
-            }
-        }
-    });
-
-    slider.events.on('transitionEnd', (info) => {
-        if (activeSlideElemwent) {
-            activeSlideElemwent.innerText = info.displayIndex;
-        }
-    });
-});
-
-const speakersGridSlider = document.querySelectorAll('.js-speakers-slider');
-speakersGridSlider.forEach(el => {
-    const totalSlidesElement = el.parentElement.parentElement.querySelector('.speakers_slider-counter-total');
-    const activeSlideElemwent = el.parentElement.parentElement.querySelector('.speakers_slider-counter-active');
-
-    const slider = tns({
-        container: el,
-        items: 1.1,
-        gutter: 16,
-        mouseDrag: true,
-        autoplay: false,
-        nav: false,
-        controlsPosition: 'bottom',
-        controls: true,
-        loop: false,
-        responsive: {
-            1070: {
-                disable: true
-            },
-        },
-        onInit: (info) => {
-            if (totalSlidesElement) {
-                totalSlidesElement.innerText = info.slideCount;
-            }
-        }
-    });
-
-    slider.events.on('transitionEnd', (info) => {
-        if (activeSlideElemwent) {
-            activeSlideElemwent.innerText = info.displayIndex;
-        }
-    });
-});
-
-const speakersSlider = document.querySelectorAll('.speakers_slider');
-speakersSlider.forEach(el => {
-    const totalSlidesElement = el.parentElement.parentElement.querySelector('.speakers_slider-counter-total');
-    const activeSlideElemwent = el.parentElement.parentElement.querySelector('.speakers_slider-counter-active');
-
-    const slider = tns({
-        container: el,
-        items: 1.1,
-        gutter: 16,
-        mouseDrag: true,
-        autoplay: false,
-        nav: false,
-        controlsPosition: 'bottom',
-        controls: true,
-        loop: false,
-        responsive: {
-            1070: {
-                autoWidth: true,
-                gutter: 20,
-                items: 3.2
-            },
-            1440: {
-                autoWidth: true,
-            }
-        },
-        onInit: (info) => {
-            if (totalSlidesElement) {
-                totalSlidesElement.innerText = info.items - 1;
-            }
-        }
-    });
-
-    slider.events.on('transitionEnd', (info) => {
-        if (activeSlideElemwent) {
-            activeSlideElemwent.innerText = info.displayIndex;
-        }
-    });
-});
-
-
-const eventsGalleryElements = document.querySelectorAll('.events_gallery');
-eventsGalleryElements.forEach(el => {
-    const gallery = tns({
-        container: el,
-        mode: 'gallery',
-        items: 1,
-        animateIn: "jello",
-        autoplay: false,
-        nav: false,
-        controls: false,
-        loop: false,
-        navContainer: '.events_slider',
-    });
-
-    const eventsSliderElement = el.parentElement.parentElement.parentElement.querySelector('.events_slider');
-    if (eventsSliderElement) {
-        const slider = tns({
-            container: eventsSliderElement,
-            autoWidth: true,
-            items: 1.1,
-            gutter: 24,
-            mouseDrag: true,
-            autoplay: false,
-            nav: false,
-            controls: false,
-            loop: true,
-            responsive: {
-                1024: {
-                    gutter: 100,
-                }
-            },
-            onInit: () => {
-                eventsSliderElement.querySelector('.tns-slide-active .events_slider-tab[data-tab="1"]').classList.add('active');
-            }
-        });
-
-        const slides = eventsSliderElement.querySelectorAll('.events_slider-tab');
-        slides.forEach((slide) => {
-            slide.addEventListener('click', function (e) {
-                const index = parseInt(e.target.dataset.tab);
-                gallery.goTo(index - 1);
-                slider.goTo(index - 1);
-
-                slides.forEach(el => el.classList.remove('active'))
-                const { displayIndex } = gallery.getInfo();
-                eventsSliderElement.querySelector(`.tns-slide-active .events_slider-tab[data-tab="${displayIndex}"]`).classList.add('active');
-            })
-        });
-    }
-})
 
 // menu
 const menuToggleElement = document.querySelector('.menu-toggle');
@@ -307,67 +114,6 @@ popupCloseElements.forEach(el => el.addEventListener('click', (e) => {
     closePopup();
 }));
 
-/* desktop-only images */
-const imagesWihoutSrc = document.querySelectorAll('img[data-src]');
-if (!isMobile) {
-    imagesWihoutSrc.forEach((img) => {
-        img.src = img.dataset.src;
-    });
-}
-
-/* Number input */
-const inputNumberElements = document.querySelectorAll('.input-number');
-inputNumberElements.forEach((el) => {
-    const minusButton = el.querySelector('button:first-child');
-    const plusButton = el.querySelector('button:last-child');
-    const countElement = el.querySelector('span');
-
-    minusButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        const count = parseInt(countElement.innerText)
-        countElement.innerText = count - 1;
-    })
-    plusButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        const count = parseInt(countElement.innerText)
-        countElement.innerText = count + 1;
-    })
-});
-
-/* Cart */
-/* checkout tickets collapse */
-const checkoutTicketTitleElements = document.querySelectorAll('.checkout_position-title');
-checkoutTicketTitleElements.forEach((el) => {
-    el.addEventListener('click', (e) => {
-        e.preventDefault();
-        e.target.classList.toggle('active');
-    })
-});
-
-/* filters toggle */
-const filtersToggleElement = document.querySelector('.filters_toggle');
-if (filtersToggleElement) {
-    function toggleFiltersPopup(e) {
-        e.preventDefault();
-        document.querySelector('.filters_content').classList.toggle('opened');
-
-        if (fadeElement) {
-            fadeElement.classList.toggle('opened');
-        }
-    }
-
-    filtersToggleElement.addEventListener('click', toggleFiltersPopup);
-    document.querySelector('.filters_close').addEventListener('click', toggleFiltersPopup);
-}
-
-/* filters collapse */
-const filtersSectionTitleElements = document.querySelectorAll('.filters_section-title');
-filtersSectionTitleElements.forEach((el) => {
-    el.addEventListener('click', (e) => {
-        e.preventDefault();
-        e.target.classList.toggle('inactive');
-    })
-});
 
 /* Tabs */
 function initTabs() {
@@ -396,68 +142,33 @@ function initTabs() {
 
 initTabs()
 
-/* showcase video */
-const showcaseVideoSource = document.querySelectorAll('.showcase_video source')
-let currentVideoUrl
-function switchVideo(el) {
-    function reloadVideo(url) {
-        el.parentElement.pause()
-        el.setAttribute('src', url);
-        el.parentElement.load();
-        el.parentElement.play();
-        currentVideoUrl = url;
-    }
-
-    if (window.innerWidth > 1044) {
-        if (currentVideoUrl !== el.dataset.url) {
-            reloadVideo(el.dataset.url);
-        }
-        return
-    }
-    
-    if (currentVideoUrl !== el.dataset.urlMobile) {
-        reloadVideo(el.dataset.urlMobile);
-    }
-}
-showcaseVideoSource.forEach(el => {
-    switchVideo(el);
-    currentVideoUrl = el.getAttribute('src');
-    window.addEventListener('resize', () => switchVideo(el))
-})
-
-
-
-/* subscribe form */
-const subscribeFormElement = document.querySelector('.footer_form');
-if (subscribeFormElement) {
-    subscribeFormElement.addEventListener('submit', (e) => {
-        e.preventDefault();
-        e.target.classList.add('hidden');
-        subscribeFormElement.parentElement.querySelector('.footer_form-success').classList.add('shown');
-    });
-}
-
 /* cookies */
 if (Cookies) {
     const hasCookies = Cookies.get('CookieNotificationCookie');
-    
     const cookiesBanner = document.querySelector('.cookies');
-    const cookiesAcceptButton = document.querySelector('.cookies_button');
+    const cookiesAcceptButton = document.querySelector('[data-cookies="accept"]');
+    const cookiesDenyButton = document.querySelector('[data-cookies="deny"]');
     
     if (cookiesAcceptButton) {
         cookiesAcceptButton.addEventListener('click', function (e) {
             e.preventDefault();
-    
             cookiesBanner.style.display = 'none';
             Cookies.set('CookieNotificationCookie', 'true', { expires: 365 });
         });
     }
-    
+    if (cookiesDenyButton) {
+        cookiesDenyButton.addEventListener('click', function (e) {
+            e.preventDefault();
+            cookiesBanner.style.display = 'none';
+            Cookies.set('CookieNotificationCookie', 'false', { expires: 365 });
+        });
+    }
+    console.log(cookiesBanner, !hasCookies);
+
     if (cookiesBanner && !hasCookies) {
-        cookiesBanner.style.display = 'block';
+        cookiesBanner.style.display = 'flex';
     }
 }
-
 
 /* Accordion */
 const accordionElements = document.querySelectorAll('.accordion');
@@ -506,49 +217,6 @@ accordionElements.forEach(accordion => {
     }));
 });
 
-/* popup form */
-const popupFormTriggers = document.querySelectorAll('.js-popup-form-trigger');
-
-function toggleFormPopup(e) {
-    e.preventDefault();
-
-    const form = e.target.dataset.form;
-
-    document.querySelector(`.js-form-popup[data-form="${form}"]`).classList.toggle('opened');
-    if (fadeElement) {
-        fadeElement.classList.toggle('opened');
-    }
-}
-
-popupFormTriggers.forEach((trigger) => {
-    trigger.addEventListener('click', toggleFormPopup);
-});
-
-console.log(document.querySelector('.form-popup_close'));
-document.querySelectorAll('.form-popup_close').forEach(el => el.addEventListener('click', closeAllOpened));
-
-/* unavailable ticketss form */
-const unavailableFormLink = document.querySelector('.js-tickets-unavailable-link');
-if (unavailableFormLink) {
-    unavailableFormLink.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.body.classList.remove('menu-opened');
-        document.body.classList.add('special-menu-opened');
-    });
-}
-
-const unavailableFormSubmit = document.querySelector('.js-unavailable-form-submit');
-if (unavailableFormSubmit) {
-    unavailableFormSubmit.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        // form sending logic
-
-        document.body.classList.remove('special-menu-opened');
-    });
-}
-
-
 /* appaerance animation */
 const animatedElements = document.querySelectorAll('.js-animation');
 
@@ -576,8 +244,3 @@ if (animatedElements.length) {
         observer.observe(el);
     })
 }
-
-// .speakers cards
-document.querySelectorAll('.speakers-card').forEach(el => el.addEventListener('click', (e) => {
-    e.currentTarget.classList.toggle('active')
-}))
