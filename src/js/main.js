633e5c1b5ae42581960899ba018abaa47fe3a133
Pavel Korol <korol.paul@gmail.com>
2023-11-02 00:04:28 +0300

    about

--- src/js/main.js
+++ src/js/main.js
@@ -16,25 +16,25 @@ showcaseSlider.forEach(el => {
         navPosition: 'bottom',
         controls: false,
         loop: false,
+        mode: 'gallery',
         responsive: {
             768: {
                 nav: false,
-                mode: 'gallery',
             }
         },
         onInit: () => {
             document.querySelectorAll('.showcase_button').forEach((element) => element.addEventListener('mouseenter', (e) => {
-                console.log(Number(e.target.dataset.slide))
-                slider.goTo(Number(e.target.dataset.slide))
+                slider.goTo(Number(e.target.dataset.slide) - 1)
             }))
         }
     });
 });
 
 const solutionsSlider = document.querySelectorAll('.solutions-map_slider');
-solutionsSlider.forEach(el => {
-    tns({
-        container: el,
+const solutionsSliderButtons = document.querySelectorAll('.solutions-map_slider-button');
+solutionsSlider.forEach(sliderElement => {
+    const slider = tns({
+        container: sliderElement,
         items: 1,
         gutter: 16,
         mouseDrag: true,
@@ -43,19 +43,134 @@ solutionsSlider.forEach(el => {
         navPosition: 'bottom',
         controls: false,
         loop: false,
+        mode: 'gallery',
+        animateIn: 'tns-goBackIn',
+        animateOut: 'tns-goBackOut',
+        speed: 800,
         responsive: {
             768: {
-                navContainer: '.solutions-map_slider-buttons'
+                nav: false,
+            }
+        },
+        onInit: () => {
+            solutionsSliderButtons.forEach((element) => element.addEventListener('click', (e) => {
+                slider.goTo(Number(e.target.dataset.slide) - 1);
+                solutionsSliderButtons.forEach(el => el.classList.remove('active'));
+                e.target.classList.add('active');
+            }));
+        }
+    });
+
+    const wrapper = sliderElement.parentElement;
+
+    if (wrapper) {
+        let isAnimated = false;
+
+        function handleCarouselScroll(isForward) {
+            if (isAnimated) {
+                return;
+            }
+
+            isAnimated = true
+            setTimeout(() => {
+                isAnimated = false
+            }, 200);
+
+            const {slideCount, index} = slider.getInfo();
+
+            if (isForward) {
+                if (index >= slideCount - 1 ) {
+                    document.removeEventListener(wheelEvent, preventScroll, { passive: false });
+                    return;
+                }
+
+                slider.goTo('next');
+                solutionsSliderButtons.forEach(el => el.classList.remove('active'));
+                solutionsSliderButtons[index + 1].classList.add('active');
+            } else {
+                if (index === 0) {
+                    document.removeEventListener(wheelEvent, preventScroll, { passive: false });
+                    return;
+                }
+
+                slider.goTo('prev');
+                solutionsSliderButtons.forEach(el => el.classList.remove('active'));
+                solutionsSliderButtons[index - 1].classList.add('active');
             }
         }
+
+        function preventScroll(e) {
+            e.preventDefault()
+
+            handleCarouselScroll(e.deltaY > 0)
+        }
+
+        const observerCallback = function (e) {
+            const { boundingClientRect, intersectionRatio } = e[0];
+            // const ratio = boundingClientRect.height / 2 - boundingClientRect.y;
+
+            if (intersectionRatio > 0.9) {
+                document.addEventListener(wheelEvent, preventScroll, { passive: false })
+            } else {
+                document.removeEventListener(wheelEvent, preventScroll, { passive: false })
+            }
+        };
+
+        const observer = new IntersectionObserver(observerCallback, {
+            rootMargin: '0px 0px 0px 0px',
+            threshold: thresholdSteps,
+            root: null
+        });
+        observer.observe(wrapper);
+    }
+});
+
+const casesSlider = document.querySelectorAll('.cases_items');
+casesSlider.forEach(el => {
+    const slider = tns({
+        container: el,
+        items: 1,
+        gutter: 16,
+        mouseDrag: true,
+        autoplay: false,
+        nav: false,
+        navPosition: 'bottom',
+        controls: true,
+        controlsPosition: 'bottom',
+        loop: true,
+        mode: 'gallery',
+        animateIn: 'tns-goBackIn',
+        animateOut: 'tns-goBackOut',
+        speed: 800,
     });
 });
 
-const teamSlider = document.querySelectorAll('.team-slider');
+const clientsMapSlider = document.querySelectorAll('.clients-map_slider');
+clientsMapSlider.forEach(el => {
+    const slider = tns({
+        container: el,
+        mouseDrag: true,
+        autoplay: false,
+        nav: false,
+        navPosition: 'bottom',
+        controls: true,
+        controlsPosition: 'top',
+        loop: true,
+        mode: 'gallery',
+        speed: 800,
+        responsive: {
+            1200: {
+                disable: true,
+            }
+        },
+    });
+});
+
+const teamSlider = document.querySelectorAll('.team-slider_items');
 teamSlider.forEach(el => {
     tns({
         container: el,
-        items: 1,
+        items: 1.6,
         gutter: 14,
         mouseDrag: true,
         autoplay: false,
@@ -69,6 +184,26 @@ teamSlider.forEach(el => {
             },
             1200: {
                 items: 4.5,
+            }
+        }
+    });
+});
+
+const teamGridSlider = document.querySelectorAll('.team-grid_items');
+teamGridSlider.forEach(el => {
+    tns({
+        container: el,
+        items: 1.6,
+        gutter: 14,
+        mouseDrag: true,
+        autoplay: false,
+        nav: false,
+        controls: true,
+        controlsPosition: 'bottom',
+        loop: false,
+        responsive: {
+            768: {
+                disable: true,
             }
         }
     });
@@ -247,22 +382,6 @@ accordionElements.forEach(accordion => {
     
     
     accordionButtons.forEach(el => el.addEventListener('click', toggle));
-    
-    const accordionTabButtons = accordion.querySelectorAll('.accordion_tab-button');
-    const accordionTabContent = accordion.querySelectorAll('.accordion_tab-content');
-
-    accordionTabButtons.forEach(el => el.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        if (e.target.classList.contains('active')) {
-            return;
-        }
-
-        accordionTabButtons.forEach(el => el.classList.remove('active'));
-        accordionTabContent.forEach(el => el.classList.remove('active'));
-        e.target.classList.add('active');
-        e.target.nextElementSibling.classList.add('active');
-    }));
 });
 
 /* appaerance animation */
@@ -300,8 +419,28 @@ if (!isMobile && prospectsElement) {
         const {height, top, bottom} = prospectsElement.getBoundingClientRect();
         const screenHeight = screen.availHeight;
 
-        console.log(height, top, (((height - top) / (height)) * 100) - 100);
-        prospectsNumbersElement.style.transform = `rotate(${Math.round((((((height - top) / height) * 100) - 100)) * -3.6)}deg)`
+        const angle = Math.round((((((height - top) / height) * 100) - 100)) * -3.6);
+        const onePercent = 100 / 300;
+
+        if (angle < 0) {
+            prospectsNumbersElement.style.transform = `rotate(${angle}deg)`;
+            prospectsNumberElements.forEach((el, i) => {
+                const scale = ((onePercent * Math.abs(angle)) / 100) - angle * i;
+                console.log(scale);
+                const finalScale = (() => {
+                    if (scale < .2) {
+                        return .2;
+                    }
+                    if (scale > 1) {
+                        return 1;
+                    }
+
+                    return scale;
+                })();
+
+                el.style.transform = `rotate(${-angle}deg) scale(${finalScale})`;
+            });
+        }
 
         // const elementBottom = offsetTop + clientHeight - screen.availHeight + 200;
 // 
@@ -309,4 +448,40 @@ if (!isMobile && prospectsElement) {
         //     console.log(`${(clientHeight / (offsetTop - scrollTop)) * 3.6 }`);
         // }
     })
+}
+
+/* blinks animation */
+const blinksElement = document.querySelector('.blinks');
+if (blinksElement) {
+    document.addEventListener(wheelEvent, (e) => {
+        if (e.deltaY > 0) {
+            blinksElement.classList.remove('blinks__down');
+            blinksElement.classList.add('blinks__up');
+        } else {
+            blinksElement.classList.add('blinks__down');
+            blinksElement.classList.remove('blinks__up');
+        }
+    });
+}
+
+/* clients map */
+const clientsMapButtons = document.querySelectorAll('.clients-map_item-button');
+if (clientsMapButtons.length) {
+    let activePlace = 1;
+
+    const clientMapItems = document.querySelectorAll('.clients-map_slider .tns-item:not(.tns-slide-cloned)');
+    clientMapItems[activePlace - 1].classList.add('active');
+    document.querySelector('.clients-map').dataset.place = activePlace;
+
+    clientsMapButtons.forEach(button => button.addEventListener('click', (e) => {
+        activePlace = Number(e.target.dataset.place);
+
+        clientsMapButtons.forEach(el => el.classList.remove('active'));
+        clientMapItems.forEach(el => el.classList.remove('active'));
+
+        clientMapItems[activePlace - 1].classList.add('active');
+        clientMapItems[activePlace - 1].querySelector('.clients-map_item-button').classList.add('active');
+
+        document.querySelector('.clients-map').dataset.place = activePlace;
+    }));
 }
